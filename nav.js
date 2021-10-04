7c8c14c8f5250fdc69ee53235313cca587b67e9b
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-10-04 16:00:14 -0400

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,12 @@
+ 	    console.log ("Images on this website should not be copied.");
+            function nav () {
+                document.getElementById ("nav").style.display = "none";
+                document.getElementById ("navigation").style.marginLeft = "0px";
+                document.getElementById ("nav2").style.display = "block";
+            }
+		
+	    function navback () {
+                document.getElementById ("nav").style.display = "block";
+                document.getElementById ("navigation").style.marginLeft = "-100%";
+                document.getElementById ("nav2").style.display = "none";
+            }
