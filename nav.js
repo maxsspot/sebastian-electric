b7562a6801c7121d1319c5e735a76a2b518a12ad
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-10-28 07:17:39 -0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -14,3 +14,31 @@
 	    function loadedpage() {
 		document.getElementById ("loader").style.visibility = "hidden";
 	    }	
+
+const menu = document.querySelector(".menu");
+let menuVisible = false;
+
+const toggleMenu = command => {
+  menu.style.display = command === "show" ? "block" : "none";
+  menuVisible = !menuVisible;
+};
+
+const setPosition = ({ top, left }) => {
+  menu.style.left = `${left}px`;
+  menu.style.top = `${top}px`;
+  toggleMenu("show");
+};
+
+window.addEventListener("click", e => {
+  if(menuVisible)toggleMenu("hide");
+});
+
+window.addEventListener("contextmenu", e => {
+  e.preventDefault();
+  const origin = {
+    left: e.pageX,
+    top: e.pageY
+  };
+  setPosition(origin);
+  return false;
+});
