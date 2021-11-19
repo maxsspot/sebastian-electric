f28e744687c205fc1a4e20db2c8687239fa77557
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-11-19 11:10:33 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,15 +1,19 @@
  	    console.log ("Images on this website should not be copied.");
             document.getElementById('nav').onclick = function nav () {
-                document.getElementById ("nav").style.display = "none";
+
+            }
+
+document.getElementById('nav').onclick = function() {
+                   document.getElementById ("nav").style.display = "none";
                 document.getElementById ("navigation").style.marginLeft = "0px";
                 document.getElementById ("nav2").style.display = "block";
-            }
-		
-	    document.getElementById('nav2').onclick = function navback () {
-                document.getElementById ("nav").style.display = "block";
+}
+
+document.getElementById('nav2').onclick = function() {
+                                   document.getElementById ("nav").style.display = "block";
                 document.getElementById ("navigation").style.marginLeft = "-100%";
                 document.getElementById ("nav2").style.display = "none";
-            }
+}
 
 	    function loadedpage() {
 		document.getElementById ("loader").style.visibility = "hidden";
