e0d6b1deaf5c8f1cf62a77aec86d9c78a69bd8bc
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-12-02 17:30:19 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,7 +1,3 @@
-document.getElementById ("nav").onclick = function() {nav()};
-document.getElementById ("nav2").onclick = function() {navback()};
-document.getElementById("nav").addEventListener("click", nav);
-
 function navback () {
 	document.getElementById ("nav").style.display = "block";
         document.getElementById ("navigation").style.marginLeft = "-100%";
