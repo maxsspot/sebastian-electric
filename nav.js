557d1b051f244e2ca47b38464ef133df59374e52
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-12-05 17:10:16 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,15 +1,15 @@
+document.getElementById ("navigation").style.opacity = "0";
+
 function navback () {
 	document.getElementById ("nav").style.display = "block";
         document.getElementById ("navigation").style.marginLeft = "-100%";
+	document.getElementById ("navigation").style.opacity = "0";
         document.getElementById ("nav2").style.display = "none";
 }	
 
 function nav () {
    	document.getElementById ("nav").style.display = "none";
         document.getElementById ("navigation").style.marginLeft = "0px";
+	document.getElementById ("navigation").style.opacity = "1";
         document.getElementById ("nav2").style.display = "block";
 }	
-
-
-$('#nav').click(opennav);
-$('#nav2').click(navback);
