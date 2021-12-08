c6b92a4c432dcdaabc307bfacef39aeff13208f1
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-12-08 12:40:24 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -17,3 +17,10 @@ self.addEventListener('install', e => {
     .then(cache => cache.addAll(filesToCache))
   );
 });
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+    .then(response => response ? response : fetch(e.request))
+  )
+});
