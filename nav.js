5f0fc8d5d39e2719d21fc65e758331f3e4b3d5b1
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-12-12 09:02:33 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -22,7 +22,7 @@ const element3 = document.getElementById('nobot')
 
 element3.addEventListener("click", () => {
 	document.getElementById ("nobot").style.fontWeight = "bold";
-	document.getElementById ("clickhere").style.display = "none";
+	document.getElementById ("verifiy").innetHTML = "<i class = 'fas fa-check'></i>";
 	document.getElementById ("submitform").disabled = false;
 });
 
